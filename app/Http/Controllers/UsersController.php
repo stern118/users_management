0d76d962f5bcc893e299beb885f74b8d5a672ac3
a-kanemitsu <a-kanemitsu@probizmo.co.jp>
2019-11-18 14:31:51 +0900

    【機能修正】ユーザ新規登録 パスワードを保存する際に、ハッシュ化するように変更。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 
 class UsersController extends Controller {
-    
+
     public function displayUsers() {
     	$records = User::all();
     	return view('users', ['records' => $records]);
@@ -15,7 +15,7 @@ class UsersController extends Controller {
     public function createUser(Request $request) {
     	$user = new User();
     	$user->user_id  = $request->user_id;
-    	$user->password = $request->password;
+    	$user->password = Hash::make($request->password);
     	$user->name     = $request->name;
     	$user->email    = $request->email;
     	$user->save();
